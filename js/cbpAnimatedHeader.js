e09c246186bc3929c89a53f6fac5ed03f5486186
ronaknnathani <ronaknnathani@gmail.com>
2015-01-18 02:05:57 -0500

    Updates

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -13,7 +13,7 @@ var cbpAnimatedHeader = (function () {
     var docElem = document.documentElement,
 		header = document.querySelector('.navbar-default'),
 		didScroll = false,
-		changeHeaderOn = 100;
+		changeHeaderOn = 40;
 
     function init() {
         window.addEventListener('scroll', function (event) {
